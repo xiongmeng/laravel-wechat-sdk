b71e867025192f11d1fde8114b32fbf8acd13299
cooper <huanghua581@126.com>
2014-08-20 11:47:59 +0800

    验证请求

--- src/routes.php
+++ src/routes.php
@@ -7,7 +7,7 @@
 
 // 测试
 //
-Route::post('api/wechat/test', array(
+Route::any('api/wechat/test', array(
     'as'     => 'wechat.test',
     'uses'   => 'Cooper\Wechat\Controllers\WechatController@test',
 ));
